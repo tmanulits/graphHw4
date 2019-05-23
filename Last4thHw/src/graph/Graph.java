0f83db150c4d4744b704c6aee948b6a833254fb5
Timothy Manulits <tmanulits@gmail.com>
2019-05-22 23:15:36 -0700

    hg

--- Last4thHw/src/graph/Graph.java
+++ Last4thHw/src/graph/Graph.java
@@ -1,5 +1,45 @@
 package graph;
 
-public class Graph {
+import java.util.*;
 
+public class Graph {
+	class Edge {
+		int v,w;
+		public Edge(int v, int w) {
+			this.v = v;
+			this.w = w;
+		}
+		
+		public String toString() {
+			return "("+v+","+w+")";
+		}
+	}
+
+	public static final int MAXIMUM = 0;
+	
+	List<Edge> G[];
+	
+	public Graph(int n) {
+		G = new LinkedList[n];
+		
+		for (int i = 0; i < G.length; i++) {
+			G[i] = new LinkedList<Edge>();
+		}
+	}
+	@Override
+	public String toString() {
+		String result = "";
+		for (int i = 0; i < G.length; i++) {
+			result += i + "=>" + G[i] + "\n";
+		}
+		return result;
+	}
+	public Set neighbors(Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public char[] get_label(int v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- Last4thHw/src/graph/NetworkGraph.java
+++ Last4thHw/src/graph/NetworkGraph.java
@@ -1,5 +1,59 @@
 package graph;
 
-public class NetworkGraph {
+import java.util.*;
 
+public class NetworkGraph {
+	public void rec_dfs(int v, Graph g, boolean[] marked) {
+		
+		Set connections = g.neighbors(v);
+	    marked[v] = true;  // Mark vertex v
+	    System.out.println(g.get_label(v));   //Do work on each vertex here
+	    
+	    // Traverse all the neighbors, looking for unmarked vertices:
+	    for (Integer x: connections) {
+	    		if (!marked[x]) //item not marked
+	        rec_dfs(x, g, marked);
+	    		
+	    		} 
+	}
+	
+	void depth_first(Graph g, int start) {
+		
+	    boolean[] marked = new boolean[g.MAXIMUM];
+	    rec_dfs(start, g, marked);
+	    
+	}
+	
+	void breadth_first(Graph g, int start) {
+		
+	    boolean[] marked = new boolean[g.MAXIMUM];
+	    Set connections;
+	    Queue vertex_queue = new LinkedList();
+	    marked[start] = true;
+	    System.out.println(g.get_label(start));
+	    vertex_queue.add(start);
+	    
+	    do {
+	    		//Do work on first vertex here
+	    		connections = g.neighbors(vertex_queue.peek( ));
+	    		vertex_queue.remove();
+	    		// Mark and process the unmarked neighbors,
+	    		// and place them in the queue.
+	    		for (Integer x: connections) {
+	    			if (!marked[x]) {
+	    				marked[x] = true;
+	    				System.out.println(g.get_label(x)); //Do work on next vertex here
+	    				vertex_queue.add(x);
+	    			} 
+	    		}
+	
+	    }
+	
+	    while (!vertex_queue.isEmpty() );
+	} 
+	
+	public static void main(String[] args) {
+		List<Integer> l[] = new LinkedList[10];
+	}
 }
+	 
